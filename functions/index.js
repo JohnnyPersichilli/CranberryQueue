0db7eaf7640d3935022e6fa1ba8c9b9a225ea091
rolflocher <rolflocher47@gmail.com>
2019-09-17 19:01:21 -0400

    Init functions

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.updateNetVotes = functions.firestore
+    .document('songs/{songId}')
+    .onUpdate((change, context) => {
+      // Get an object representing the document
+      // e.g. {'name': 'Marie', 'age': 66}
+      const newValue = change.after.data();
+      console.log(newValue)
+
+      // ...or the previous value before this update
+      const previousValue = change.before.data();
+      console.log(previousValue)
+
+      // access a particular field as you would any JS property
+      const name = newValue.name;
+
+      // perform desired operations ...
+    });
