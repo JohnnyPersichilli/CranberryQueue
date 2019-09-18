d1b45020964f7edf483a025e61efc266e771c48e
rolflocher <rolflocher47@gmail.com>
2019-09-17 20:30:54 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,18 +10,42 @@ const functions = require('firebase-functions');
 
 exports.updateNetVotes = functions.firestore
     .document('song/{songId}/upvoteUsers/{uid}')
-    .onUpdate((change, context) => {
-      // Get an object representing the document
-      // e.g. {'name': 'Marie', 'age': 66}
-      const newValue = change.after.data();
-      console.log(context.params.songId);
+    .onCreate((snap, context) => {
 
-      // ...or the previous value before this update
-      const previousValue = change.before.data();
-      console.log(context.params.uid);
+    const songId = context.params.songId;
+    const uid = context.params.uid;
 
-      // access a particular field as you would any JS property
-      const name = newValue.name;
+    db.collection('song').doc(songId).get()
+    .then(doc => {
+        const queueId = doc.data().queueId
+        db.collection('song').doc(songId).collection('downvoteUsers').get()
+        .then(snapshot => {
+            if (snapshot.empty) {
+                return;
+            }
+            var isDup = false;
+            snapshot.forEach(doc => {
+                if (doc.id == uid) {
+                    isDup = false;
+                }
+            });
+            if (isDup) {
+                db.collection('song').doc(songId).collection('downvoteUsers').doc(uid).delete()
+                .then( val => {
 
-      // perform desired operations ...
+                })
+            }
+            else {
+                db.collection('song').doc(songId).collection('upvoteUsers').get()
+                    .then(snap => {
+                        const netvotes = snap.length - snapshot.length;
+                    db.collection('playlist').doc(queueId).collection('songs').doc(songId).set({
+                        votes: netvotes
+                    }, {merge: true})
+                })
+                
+            }
+        })
+    })
+
     });
