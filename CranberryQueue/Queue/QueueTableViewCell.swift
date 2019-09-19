c98b1459eb2852ca340ead6b736653938a57159b
Matt Innocenzo <minnoce1@villanova.edu>
2019-09-19 11:06:34 -0400

    Arrows on QueueScreen are now oriented correctly

--- CranberryQueue/Queue/QueueTableViewCell.swift
+++ CranberryQueue/Queue/QueueTableViewCell.swift
@@ -25,6 +25,7 @@ class QueueTableViewCell: UITableViewCell {
     
     @IBOutlet var shadowView: UIView!
     
+    
     var uid: String? = nil
     
     var songId: String? = nil
@@ -42,10 +43,13 @@ class QueueTableViewCell: UITableViewCell {
         let upvoteTap = UITapGestureRecognizer(target: self, action: #selector(upvoteTapped))
         upvoteButtonImageView.addGestureRecognizer(upvoteTap)
         upvoteButtonImageView.isUserInteractionEnabled = true
+        upvoteButtonImageView.transform = CGAffineTransform(rotationAngle: 270*3.1415926/180)
+        
         
         let downvoteTap = UITapGestureRecognizer(target: self, action: #selector(downvoteTapped))
         downvoteButtonImageView.addGestureRecognizer(downvoteTap)
         downvoteButtonImageView.isUserInteractionEnabled = true
+        downvoteButtonImageView.transform = CGAffineTransform(rotationAngle: 90*3.1415926/180)
         
         shadowView.layer.shadowRadius = 15
         shadowView.layer.shadowColor = UIColor.black.cgColor
