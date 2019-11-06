a948df578b854558f8c6f9d02a5987d486716e27
JohnnyPersichilli <jpersich@villanova.edu>
2019-11-06 01:18:13 -0500

    applied shadows to all searchTableCells and made UI the same for all views

--- CranberryQueue/Queue/SegmentedModal/SearchTableViewCell.swift
+++ CranberryQueue/Queue/SegmentedModal/SearchTableViewCell.swift
@@ -20,14 +20,61 @@ class SearchTableViewCell: UITableViewCell {
     
     var song = Song()
     
+    var shadowLayer = CALayer()
+    var gradientLayer = CAGradientLayer()
+    var shadOpacity: Float = 1
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     
         albumImageView.layer.cornerRadius = albumImageView.frame.height/2
         albumImageView.clipsToBounds = true
         
-        self.layer.borderColor = UIColor.clear.cgColor
+        self.addShadow()
+    }
+    
+    func addShadow() {
+        if (contentView.layer.sublayers?.contains(shadowLayer) ?? false) {
+            return
+        }
         
+        shadowLayer = CALayer()
+        let mutablePath = CGMutablePath()
+        let maskLayer = CAShapeLayer()
+
+        let usableBounds = CGRect(x: contentView.frame.minX + 10, y: contentView.frame.minY + 1 , width: contentView.frame.width - 20, height: contentView.frame.height - 2)
+        let xOffset = CGFloat(2.3)
+        let yOffset = CGFloat(4)
+        let shadowOffset = CGSize(width: xOffset, height: yOffset)
+        let shadowOpacity = shadOpacity
+        let shadowRadius = CGFloat(3)
+        let shadowPath = UIBezierPath(rect: usableBounds).cgPath
+        let shadowColor = UIColor.black
+        let shadowFrame = usableBounds.insetBy(dx: -2 * shadowRadius, dy: -2 * shadowRadius).offsetBy(dx: xOffset, dy: yOffset)
+        let shadowRect = CGRect(origin: .zero, size: shadowFrame.size)
+        let shadowTransform = CGAffineTransform(translationX: -usableBounds.origin.x - xOffset + 2 * shadowRadius, y: -usableBounds.origin.y - yOffset + 2 * shadowRadius)
+
+        shadowLayer.shadowOffset = shadowOffset
+        shadowLayer.shadowOpacity = shadowOpacity
+        shadowLayer.shadowRadius = shadowRadius
+        shadowLayer.shadowPath = shadowPath
+        shadowLayer.shadowColor = shadowColor.cgColor
+
+        mutablePath.addRect(shadowRect)
+        mutablePath.addPath(shadowLayer.shadowPath!, transform: shadowTransform)
+        mutablePath.closeSubpath()
+
+        maskLayer.frame = shadowFrame
+        maskLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        maskLayer.path = mutablePath
+
+        shadowLayer.mask = maskLayer
+        contentView.layer.insertSublayer(shadowLayer, above: layer)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.layoutSubviews()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
