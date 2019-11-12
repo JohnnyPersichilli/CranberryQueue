0341a9a77d204bfcfd943e965d760321732c2f2a
creiser2 <carlreiser4@gmail.com>
2019-11-11 21:45:32 -0500

    semi fix

--- CranberryQueue/AppDelegate.swift
+++ CranberryQueue/AppDelegate.swift
@@ -40,7 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, SPTSessionManagerDelegate
             let tokenRefreshURL = URL(string: "https://cranberryqueue.herokuapp.com/api/refresh_token") {
             self.configuration.tokenSwapURL = tokenSwapURL
             self.configuration.tokenRefreshURL = tokenRefreshURL
-            self.configuration.playURI = nil
+            self.configuration.playURI = ""
         }
         let manager = SPTSessionManager(configuration: self.configuration, delegate: self)
         return manager
